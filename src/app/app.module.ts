9658aa1d3a9a8fe07f0595a2963c2ae94a058624
Ritik Bhardwaj <ritik.bhardwaj889@gmail.com>
2022-02-12 22:17:06 +0530

    Add Routing and Sign Up page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegisterFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginFormComponent },
+      { path: 'register', component: RegisterFormComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
